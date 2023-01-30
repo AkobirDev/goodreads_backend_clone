1043b961523d4ba82de82bf4a403aa1db85feed4
Akobir Tursunov <97350561+AkobirDev@users.noreply.github.com>
2023-01-30 17:02:10 +0500

    add generic views

--- books/views.py
+++ books/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, ListView, DetailView
 from .models import Book
+
 # Create your views here.
 
-class BookListView(View):
-    def get(self, request):
-        books = Book.objects.all()
-        context = {'books': books}
-        return render(request, 'books/list.html', context)
+class BookListView(ListView):
+    template_name = 'books/list.html'
+    queryset = Book.objects.all()
+    context_object_name = 'books'
 
 
-class BookDetailView(View):
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
-        context = {'book': book}
-        return render(request, 'books/book_detail.html', context)
+# class BookListView(View):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         context = {'books': books}
+#         return render(request, 'books/list.html', context)
+
+
+class BookDetailView(DetailView):
+    template_name = 'books/book_detail.html'
+    pk_url_kwarg = 'id'
+    model = Book
+
+# class BookDetailView(View):
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#         context = {'book': book}
+#         return render(request, 'books/book_detail.html', context)
