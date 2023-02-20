3d2c75cb3bb4528a34599e8788ca224e8c49cce1
Akobir Tursunov <97350561+AkobirDev@users.noreply.github.com>
2023-02-20 15:27:00 +0500

    fix debugs

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -150,7 +150,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'akobirtursunov30@gmail.com'
-EMAIL_HOST_PASSWORD = 'akobir2004'
+EMAIL_HOST_PASSWORD = 'tlkjyqtanvgnrufb'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
